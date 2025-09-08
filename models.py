680677fb1f648a4a3b0427454287c03574917d65
MortyTsai <mortyyuhau@gmail.com>
2025-09-08 18:21:38 +0800

    擴充資料庫以支援 Re-ID 特徵持久化

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # models.py
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, LargeBinary
 from database import Base
 
 class Event(Base):
@@ -10,6 +10,7 @@ class Event(Base):
     event_type = Column(String, default="person_detected")
     video_path = Column(String, index=True)
     status = Column(String, default="unreviewed")
+    reid_features = Column(LargeBinary, nullable=True)
 
     def __repr__(self):
         return f"<Event(id={self.id}, time='{self.timestamp}', path='{self.video_path}')>"
