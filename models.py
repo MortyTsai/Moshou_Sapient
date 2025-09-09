2f11e6b15569d371475a82fdee1d00baa0d63084
MortyTsai <mortyyuhau@gmail.com>
2025-09-09 22:54:36 +0800

    新增 Person 模型並擴充 Event 外鍵

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 # models.py
-from sqlalchemy import Column, Integer, String, DateTime, func, LargeBinary
+from sqlalchemy import Column, Integer, String, DateTime, func, LargeBinary, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Event(Base):
@@ -11,6 +12,23 @@ class Event(Base):
     video_path = Column(String, index=True)
     status = Column(String, default="unreviewed")
     reid_features = Column(LargeBinary, nullable=True)
+    person_id = Column(Integer, ForeignKey("persons.id"), nullable=True, index=True)
+    person = relationship("Person")
 
     def __repr__(self):
-        return f"<Event(id={self.id}, time='{self.timestamp}', path='{self.video_path}')>"
+        return f"<Event(id={self.id}, person_id={self.person_id}, time='{self.timestamp}', path='{self.video_path}')>"
+
+class Person(Base):
+    """
+    代表一個被系統獨立識別的個體 (全域人物畫廊)。
+    """
+    __tablename__ = "persons"
+
+    id = Column(Integer, primary_key=True)
+    first_seen = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    last_seen = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    representative_feature = Column(LargeBinary, nullable=False)
+    sighting_count = Column(Integer, default=1, nullable=False)
+
+    def __repr__(self):
+        return f"<Person(id={self.id}, last_seen='{self.last_seen}', sightings={self.sighting_count})>"
