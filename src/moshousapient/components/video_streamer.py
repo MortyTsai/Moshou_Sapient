f67a048070d530f512c9cb77f297f14620effb84
MortyTsai <mortyyuhau@gmail.com>
2025-09-11 00:32:19 +0800

    新增 is_alive 方法以供消費者判斷來源狀態。

--- src/moshousapient/components/video_streamer.py
+++ src/moshousapient/components/video_streamer.py
@@ -1,4 +1,5 @@
-# video_streamer.py
+# src/moshousapient/components/video_streamer.py
+
 import subprocess
 import threading
 import numpy as np
@@ -6,10 +7,9 @@ import time
 import logging
 from ..config import Config
 
+
 class VideoStreamer:
-    """
-    使用 FFmpeg 從 RTSP 來源或本地檔案讀取影像串流，並將幀放入佇列中。
-    """
+    """使用 FFmpeg 從 RTSP 來源或本地檔案讀取影像串流，並將幀放入佇列中。"""
 
     def __init__(self, src, width, height, use_udp=True):
         self.src = src
@@ -19,15 +19,14 @@ class VideoStreamer:
         self.thread = None
         self.consumer_queue = None
         self.inference_queue = None
-        self.command = ['ffmpeg', '-hide_banner', '-loglevel', 'error']
 
+        self.command = ['ffmpeg', '-hide_banner', '-loglevel', 'error']
         if Config.VIDEO_SOURCE_TYPE == "FILE":
             logging.info(f"影像讀取器: 初始化檔案串流 (來源: {self.src})。使用 -re 參數模擬即時幀率。")
             self.command.extend(['-re', '-i', self.src])
-
         elif Config.VIDEO_SOURCE_TYPE == "RTSP":
             protocol = "UDP" if use_udp else "TCP"
-            logging.info(f"影像讀取器: 初始化 RTSP 串流 (協定: {protocol})，解析度: {width}x{height}")
+            logging.info(f"影像讀取器: 初始化 RTSP 串流 (協定: {protocol}), 解析度: {width}x{height}")
             if use_udp:
                 self.command.extend(
                     ['-rtsp_transport', 'udp', '-probesize', '5M', '-analyzeduration', '5M', '-i', self.src])
@@ -77,14 +76,13 @@ class VideoStreamer:
                     if Config.VIDEO_SOURCE_TYPE == "FILE":
                         logging.info("影像讀取器: 影片檔案已讀取完畢。")
                     break
-
         except Exception as e:
             logging.error(f"影像讀取器: 主更新迴圈發生錯誤: {e}", exc_info=True)
         finally:
             if process and process.poll() is None:
                 process.kill()
                 process.wait()
-                logging.info("影像讀取器: 已終止 FFmpeg 程序。")
+            logging.info("影像讀取器: 已終止 FFmpeg 程序。")
 
             if process and process.stderr:
                 stderr_output = process.stderr.read().decode('utf-8', errors='ignore')
@@ -101,3 +99,6 @@ class VideoStreamer:
             if self.thread.is_alive():
                 logging.warning("影像讀取器: 生產者執行緒關閉超時。")
         logging.info("影像讀取器: 生產者執行緒已停止。")
+
+    def is_alive(self):
+        return self.thread and self.thread.is_alive()
