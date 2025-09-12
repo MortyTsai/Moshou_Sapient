29d8f393baeaee3d85005d853fd63c62d7833118
MortyTsai <mortyyuhau@gmail.com>
2025-09-13 00:06:25 +0800

    更新主程式入口點以指向 core 子套件。

--- src/moshousapient/__main__.py
+++ src/moshousapient/__main__.py
@@ -2,9 +2,11 @@
 
 """
 讓 moshousapient 套件可以透過 python -m moshousapient 指令執行的主入口。
+
+這個檔案現在從 core 子套件中導入並執行主應用程式邏輯。
 """
 
-from .main import main
+from .core.main import main
 
 if __name__ == "__main__":
     main()
