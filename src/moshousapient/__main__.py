8c34c1886484e8581dd0867c63cc57c3bec164be
MortyTsai <mortyyuhau@gmail.com>
2025-10-05 20:15:49 +0800

    更新主入口點以指向新的應用協調器

--- src/moshousapient/__main__.py
+++ src/moshousapient/__main__.py
@@ -1,12 +1,11 @@
 # src/moshousapient/__main__.py
-
 """
-讓 moshousapient 套件可以透過 python -m moshousapient 指令執行的主入口。
+讓 moshousapient 套件可以透過 `python -m moshousapient` 指令執行的主入口。
 
-這個檔案現在從 core 子套件中導入並執行主應用程式邏輯。
+這個檔案現在從核心的應用協調器 (app orchestrator) 中導入並執行主應用邏輯。
 """
 
-from .core.main import main
+from .core.app_orchestrator import main
 
 if __name__ == "__main__":
     main()
